c3d150c75f69f753b02cfbe228d05e4a9fdba074
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-10 21:22:18 +0100

    render images from search on page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,13 +1,17 @@
 import '../styles/search-results.css';
 
-const SearchResults = ({ searchResults }) => {
-  let image = searchResults.img;
-
-  return (
-    <>
-      <img className='search-result-image' src={image} alt='first search result' />
-    </>
-  );
+const SearchResults = ({ results }) => {
+  if(!results.length) {
+    return <p>No results!</p>
+  } else {
+    return (
+      <>
+        {results.map((result, index) => {
+          return <img className='search-result-image' src={result} alt={`search result: ${index}`} />
+        })}
+      </>
+    );
+  }
 }
 
 export default SearchResults;
