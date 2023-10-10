45091b2d8c1b502e49f6461bf7997c6e1dc91ae7
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-10 15:31:00 +0100

    add GET request and set state in handler function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import '../styles/app.css';
 import Search from './Search';
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState();
+  
   return (
     <div className='app'>
       <img className='logo' src='https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg' alt='NASA logo' />
-      <Search />
+      <Search setSearchResults={setSearchResults} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import '../styles/search.css';
+import getImages from '../requests/getImages';
 
-const Search = () => {
-  const [value, setValue] = useState("");
+const Search = ({ setSearchResults }) => {
+  const [value, setValue] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchResults(getImages(value));
+  }
+
   return (
     <>
-      <form>
+      <form className='search-form' onSubmit={handleSubmit}>
         <input className='search-input' type="text" onChange={(e) => setValue(e.target.value)} />
         <button className='search-button' type='submit' >Search</button>
       </form>
