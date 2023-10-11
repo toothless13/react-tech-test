5fede2209bde59284e190036afaa3807a39eab79
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-11 10:25:31 +0100

    add test for SearchResults and add id for array key

--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -2,17 +2,18 @@ import { render, screen } from '@testing-library/react';
 import SearchResults from '../components/SearchResults';
 
 describe('SearchResults', () => {
-  const validProps = { img: "https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" };
+  const validProps = { results: ["https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80", "https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"] };
 
+  const { results } = validProps;
   it('renders correctly', () => {
-    const { asFragment } = render(<SearchResults searchResults={validProps} />);
+    const { asFragment } = render(<SearchResults results={results} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('renders the image to the screen', () => {
-    render(<SearchResults searchResults={validProps} />);
+    render(<SearchResults results={results} />);
 
-    expect(screen.getByAltText('first search result')).toBeInTheDocument();
+    expect(screen.getAllByTestId('search-result-image')).toBeTruthy();
   });
 });
